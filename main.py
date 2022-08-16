1b6ec6a541cfd8f8a01766fb82490d6ffc115081
felipeLx <felipealvesfinanceiro@gmail.com>
2022-08-16 20:09:44 -0300

    add state and pandas to project

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ PUB_SUB_SUBSCRIPTION = "streamlit-file-sub"
 def push_payload(payload, topic, project):        
         publisher = pubsub_v1.PublisherClient() 
         topic_path = publisher.topic_path(project, topic)        
-        data = str(payload)       
+        data = json.dumps(payload)      
         future = publisher.publish(topic_path, data=data)
         print("Pushed message to topic.")   
     
