a9525d365302c9453f8f5707ae3bdf42dce2a82b
felipeLx <felipealvesfinanceiro@gmail.com>
2022-08-16 16:21:17 -0300

    add state and pandas to project

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
+from cgi import print_environ
+from re import sub
 from numpy import place
 import pyrebase
 import streamlit as st
 from datetime import datetime
 from io import StringIO
+import firebaseConfig as fc
+import pandas as pd
 
-firebaseConfig = {
-  'apiKey': "AIzaSyDmaICrJnzE6_mkwsFLwMGDHys-a-5cL1s",
-  'authDomain': "streamlit-firebase-mapfre.firebaseapp.com",
-  'projectId': "streamlit-firebase-mapfre",
-  'databaseURL': "https://streamlit-firebase-mapfre-default-rtdb.firebaseio.com/",
-  'storageBucket': "streamlit-firebase-mapfre.appspot.com",
-  'messagingSenderId': "132852245968",
-  'appId': "1:132852245968:web:231b2521c6870cbbccc5d5",
-  'measurementId': "G-TWNT2X9R59"
-}
 
 # firebase authentication
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(fc.firebaseConfig)
 auth = firebase.auth()
 
 # database
@@ -27,57 +21,46 @@ st.set_page_config(page_title="Mapfre arquivos", page_icon="🧊")
 st.image("https://upload.wikimedia.org/wikipedia/commons/b/bf/Mapfre_logo.svg", width=200)
 
 placeholder = st.empty()
-authenticate = False
+
+# initialize state
+if 'key' not in st.session_state:
+  st.session_state.key = False
+
 user = ''
-if ~authenticate:
-  with placeholder.form(key="my-form", clear_on_submit=True):
-    choice = st.selectbox('Entrar/Registrar', ['Entrar', 'Registrar'])
+if ~st.session_state.key:
+  with placeholder.form(key="my-form"):
+    options = st.selectbox('Entrar/Registrar', ['Entrar', 'Registrar'])
     email = st.text_input('Informar e-mail')
     password = st.text_input('Informar password', type='password')
-
-    if choice == 'Registrar':
-      submit = st.form_submit_button('Registrar')
     
+    if options:
+      submit = st.form_submit_button('Enviar')
       if submit:
-        user = auth.create_user_with_email_and_password(email, password)
-        authenticate = True
-        placeholder.success('Usuário criado com sucesso')
-        placeholder.balloons()
-    
-    else:
-      enter = st.form_submit_button('Entrar')
-      if enter:  
-        user = auth.sign_in_with_email_and_password(email, password)
-        authenticate = True
-
-if user:
+        st.session_state.key = True
+        if(options == 'Registrar'):
+          #Signup 
+          user = auth.create_user_with_email_and_password(email, password)
+          st.success('Usuário criado com sucesso')
+          st.balloons()
+        else:
+          # Login
+          user = auth.sign_in_with_email_and_password(email, password)
+          db.child(user['localId']).child("Handle").set(email)
+          db.child(user['localId']).child("ID").set(user['localId'])
+          
+if st.session_state.key:
   placeholder.empty()
-# ---- MAINPAGE ----
-  st.title("Aquivo Texto Mapfre")
-  st.markdown("""---""")
+  c = st.container()
+  # ---- MAINPAGE ----
+  c.title("Aquivo Texto Mapfre")
+  c.markdown("""---""")
 
-  uploaded_file = st.file_uploader("Escolha o arquivo TXT")
-  if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+  uploaded_file = c.file_uploader("Escolha o arquivo TXT/CSV", type=["txt", "csv"], on_change=None, key="my-file", accept_multiple_files=False)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+  if uploaded_file:
+    print('subiu arquivo')
+    # c.write("Arquivo selecionado: " + uploaded_file)
+    df = pd.read_csv(uploaded_file, sep=";", encoding='latin-1')
+    c.write(df)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-      
 
-    
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
