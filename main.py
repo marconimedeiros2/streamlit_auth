321989b63bbe242a53ee74f0e848d6d2213d2fd6
felipeLx <felipealvesfinanceiro@gmail.com>
2022-08-16 22:36:03 -0300

    add state and pandas to project

--- main.py
+++ main.py
@@ -42,15 +42,11 @@ def push_payload(payload, topic, project):
 # convert csv to json
 def csv_to_json(csvFile):
     jsonArray = []  
-    #read csv file
-    with csvFile as csvf: 
-        #load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf) 
-        #convert each csv row into python dict
-        for row in csvReader: 
-            #add this python dict to json array
-            jsonArray.append(row)
-  
+    #convert each csv row into python dict
+    for row in csvFile: 
+        #add this python dict to json array
+        jsonArray.append(row)
+
     #convert python jsonArray to JSON String
     return jsonArray
           
